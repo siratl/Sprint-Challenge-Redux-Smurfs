0184b4c80a04d2c51d932c7158b43d39857e2c9c
siratl <elishubax@gmail.com>
2019-02-01 09:41:44 -0800

    Wired up reducers and created action exports. working on axios.get request

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,20 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios'
+
+export const FETCHING_SMURFS = 'FETCHING_SMURFS';
+export const FETCHING_SMURFS_SUCCESSFUL = 'FETCHING_SMURFS_SUCCESSFUL';
+export const FETCHING_SMURFS_FAILED = 'FETCHING_SMURFS_FAILED';
+export const ADDING_SMURF = 'ADDING_SMURF';
+export const ADDING_SMURF_SUCCESSFUL = 'ADDING_SMURF_SUCCESSFUL';
+export const ADDING_SMURF_FAILED = 'ADDING_SMURF_FAILED';
+export const UPDATING_SMURF = 'UPDATING_SMURF';
+export const UPDATING_SMURF_SUCCESSFUL = 'UPDATING_SMURF_SUCCESSFUL';
+export const UPDATING_SMURF_FAILED = 'UPDATING_SMURF_FAILED';
+export const DELETING_SMURF = 'DELETING_SMURF';
+export const DELETING_SMURF_SUCCESSFUL = 'DELETING_SMURF_SUCCESSFUL';
+export const DELETING_SMURF_FAILED = 'DELETING_SMURF_FAILED';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +27,8 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const fetchSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING_SMURFS })
+  axios.get('http://localhost:3333/smurfs')
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,17 +2,30 @@
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+import {
+  FETCHING_SMURFS,
+  FETCHING_SMURFS_SUCCESSFUL,
+  FETCHING_SMURFS_FAILED,
+  ADDING_SMURF,
+  ADDING_SMURF_SUCCESSFUL,
+  ADDING_SMURF_FAILED,
+  UPDATING_SMURF,
+  UPDATING_SMURF_SUCCESSFUL,
+  UPDATING_SMURF_FAILED,
+  DELETING_SMURF,
+  DELETING_SMURF_SUCCESSFUL,
+  DELETING_SMURF_FAILED,
+} from '../actions'
+
+const initialState = {
+  smurfs: [],
+
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null,
+};
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +34,97 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+const smurfReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_SMURFS:
+      return {
+        ...state,
+        smurfs: [],
+        fetchingSmurfs: true,
+        error: null,
+      };
+    case FETCHING_SMURFS_SUCCESSFUL:
+      return {
+        ...state,
+        smurfs: action.payload,
+        fetchingSmurfs: false,
+        error: null,
+      };
+    case FETCHING_SMURFS_FAILED:
+      return {
+        ...state,
+        smurfs: [],
+        fetchingSmurfs: false,
+        error: action.payload,
+      };
+    case ADDING_SMURF:
+      return {
+        ...state,
+        smurfs: [],
+        addingSmurf: true,
+        error: null,
+      };
+    case ADDING_SMURF_SUCCESSFUL:
+      return {
+        ...state,
+        smurfs: action.payload,
+        addingSmurf: false,
+        error: null,
+      };
+    case ADDING_SMURF_FAILED:
+      return {
+        ...state,
+        smurfs: [],
+        addingSmurf: false,
+        error: action.payload,
+      };
+
+    case UPDATING_SMURF:
+      return {
+        ...state,
+        smurfs: [],
+        updatingSmurf: true,
+        error: null,
+      };
+    case UPDATING_SMURF_SUCCESSFUL:
+      return {
+        ...state,
+        smurfs: action.payload,
+        updatingSmurf: false,
+        error: null,
+      };
+    case UPDATING_SMURF_FAILED:
+      return {
+        ...state,
+        smurfs: [],
+        updatingSmurf: false,
+        error: action.payload,
+      };
+    case DELETING_SMURF:
+      return {
+        ...state,
+        smurfs: [],
+        deletingSmurf: true,
+        error: null,
+      };
+    case DELETING_SMURF_SUCCESSFUL:
+      return {
+        ...state,
+        smurfs: action.payload,
+        deletingSmurf: false,
+        error: null,
+      };
+    case DELETING_SMURF_FAILED:
+      return {
+        ...state,
+        smurfs: [],
+        deletingSmurf: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default smurfReducer
