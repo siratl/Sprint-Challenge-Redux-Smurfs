f1b6c759189673beec225a5e06d7c61e17b18757
siratl <elishubax@gmail.com>
2019-02-01 09:11:30 -0800

    Wired up redux thunk in index.js and imported reducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -5,18 +5,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 import App from './components/App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
+import rootReducer from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger)),
 );
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
